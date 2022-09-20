50e22c398381c395ed84eabb22004293509e3be4
Ingo Ruhnke <grumbel@gmail.com>
2022-09-20 16:34:13 +0200

    Update defaultPackage to packages.${system}.default

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Python S-Expression Parser";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -10,17 +10,15 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages = flake-utils.lib.flattenTree rec {
+      in {
+        packages = rec {
+          default = sexp-python;
+
           sexp-python = pkgs.python3Packages.buildPythonPackage {
             name = "sexp-python";
             src = nixpkgs.lib.cleanSource ./.;
-            nativeBuildInputs = [
-            ];
-            propagatedBuildInputs = [
-            ];
           };
         };
-        defaultPackage = packages.sexp-python;
-      });
+      }
+    );
 }
